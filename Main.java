d77cb6a92c6df7a6acf137a1c0528093fb24ae24
Arman Kazi <80411035+Hacker6937@users.noreply.github.com>
2022-05-17 16:24:58 +0000

    Updated the GUI interface

--- Main.java
+++ Main.java
@@ -7,7 +7,7 @@ double number, memValue;
 char op;  
   
 String digitButtonText[] = {"1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "." };  
-String operatorButtonText[] = {"/", "sqrt", "*", "-", "+", "=" };  
+String operatorButtonText[] = {"/", "sqrt", "X", "-", "+", "=" };  
 String memoryButtonText[] = {"MC", "MR", "MS"};  
 String specialButtonText[] = {"Backspc", "C", "CE" };  
   
